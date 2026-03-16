b8e8f7b3575b373e787f6cb5bf42d3770d8c5090
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2026-03-16 17:49:17 -0300

    feat: Added uazapi chats endpoints

---
+++ src/Whatsapp/Uazapi/Endpoints/Chats/UazapiChats.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Helvetitec\Messaging\Whatsapp\Uazapi\Endpoints\Chats;
+
+use Exception;
+use Helvetitec\Messaging\Whatsapp\Uazapi\Endpoints\UazapiInstanceEndpoint;
+use Illuminate\Support\Facades\Http;
+use Nette\NotImplementedException;
+
+class UazapiChats extends UazapiInstanceEndpoint
+{
+    /**
+     * Deletes chat from number
+     *
+     * @param string $number
+     * @param boolean $deleteChatFromDb
+     * @param boolean $deleteMessagesFromDb
+     * @param boolean $deleteChatFromWhatsapp
+     * @return boolean
+     */
+    public function delete(string $number, bool $deleteChatFromDb = true, bool $deleteMessagesFromDb = true, bool $deleteChatFromWhatsapp = true): bool
+    {
+        $url = $this->root().'chat/delete';
+        $response = Http::asJson()->withHeader('token', $this->token)->post($url, [
+            "number" => $number,
+            "deleteChatDB" => $deleteChatFromDb,
+            "deleteMessagesDB" => $deleteMessagesFromDb,
+            "deleteChatWhatsApp" => $deleteChatFromWhatsapp
+        ]);
+
+        if(!$response->successful()){
+            if($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid payload! - ".$response->body());
+            }elseif($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid token!");
+            }elseif($response->status() == 404){
+                throw new Exception("[UAZAPI] Instance not found!");
+            }else{
+                $status = $response->status();
+                $body = $response->body();
+                throw new Exception("[UAZAPI] Failed with status {{ $status }}: {{ $body }}");
+            }
+        }
+
+        return true;
+    }
+
+    public function archive(string $number, bool $archive): bool
+    {
+        $url = $this->root().'chat/archive';
+        $response = Http::asJson()->withHeader('token', $this->token)->post($url, [
+            "number" => $number,
+            "archive" => $archive
+        ]);
+
+        if(!$response->successful()){
+            if($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid payload! - ".$response->body());
+            }elseif($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid token!");
+            }elseif($response->status() == 404){
+                throw new Exception("[UAZAPI] Instance not found!");
+            }else{
+                $status = $response->status();
+                $body = $response->body();
+                throw new Exception("[UAZAPI] Failed with status {{ $status }}: {{ $body }}");
+            }
+        }
+
+        return true;
+    }
+
+    public function markAsRead(string $number, bool $read): bool
+    {   
+        $url = $this->root().'chat/read';
+        $response = Http::asJson()->withHeader('token', $this->token)->post($url, [
+            "number" => $number,
+            "read" => $read
+        ]);
+
+        if(!$response->successful()){
+            if($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid payload! - ".$response->body());
+            }elseif($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid token!");
+            }elseif($response->status() == 404){
+                throw new Exception("[UAZAPI] Instance not found!");
+            }else{
+                $status = $response->status();
+                $body = $response->body();
+                throw new Exception("[UAZAPI] Failed with status {{ $status }}: {{ $body }}");
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Mutes a selected chat.
+     * Valid muteEndTime: Any positive number. 0 Removes mute. -1 sets mute to forever.
+     *
+     * @param string $number
+     * @param integer $muteEndTime
+     * @return boolean
+     */
+    public function mute(string $number, int $muteEndTime): bool
+    {
+        $url = $this->root().'chat/mute';
+        $response = Http::asJson()->withHeader('token', $this->token)->post($url, [
+            "number" => $number,
+            "muteEndTime" => $muteEndTime
+        ]);
+
+        if(!$response->successful()){
+            if($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid payload! - ".$response->body());
+            }elseif($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid token!");
+            }elseif($response->status() == 404){
+                throw new Exception("[UAZAPI] Instance not found!");
+            }else{
+                $status = $response->status();
+                $body = $response->body();
+                throw new Exception("[UAZAPI] Failed with status {{ $status }}: {{ $body }}");
+            }
+        }
+
+        return true;
+    }
+
+    public function pin(string $number, bool $pin)
+    {
+        $url = $this->root().'chat/pin';
+        $response = Http::asJson()->withHeader('token', $this->token)->post($url, [
+            "number" => $number,
+            "pin" => $pin
+        ]);
+
+        if(!$response->successful()){
+            if($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid payload! - ".$response->body());
+            }elseif($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid token!");
+            }elseif($response->status() == 404){
+                throw new Exception("[UAZAPI] Instance not found!");
+            }else{
+                $status = $response->status();
+                $body = $response->body();
+                throw new Exception("[UAZAPI] Failed with status {{ $status }}: {{ $body }}");
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Search the chats with a certain body.
+     * **NOT IMPLEMENTED**
+     *
+     * @todo Implement functionality
+     * @param array $body
+     * @throws NotImplementedException
+     */
+    public function search(array $body)
+    {
+        $url = $this->root().'chat/find';
+        $response = Http::asJson()->withHeader('token', $this->token)->post($url, $body);
+
+        if(!$response->successful()){
+            if($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid payload! - ".$response->body());
+            }elseif($response->status() == 400){
+                throw new Exception("[UAZAPI] Invalid token!");
+            }elseif($response->status() == 404){
+                throw new Exception("[UAZAPI] Instance not found!");
+            }else{
+                $status = $response->status();
+                $body = $response->body();
+                throw new Exception("[UAZAPI] Failed with status {{ $status }}: {{ $body }}");
+            }
+        }
+
+        throw new NotImplementedException();
+    }
+}
