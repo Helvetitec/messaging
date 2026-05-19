645997d7ec54c14c143c276d94e22dab1df83216
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2026-05-19 10:57:23 -0300

    fix: Fixed invalid return for HttpStatusException

--- src/Exceptions/HttpStatusException.php
+++ src/Exceptions/HttpStatusException.php
@@ -9,6 +9,6 @@ class HttpStatusException extends Exception
 {   
     public function __construct(public int $statusCode, string $message = "", int $code = 0, Throwable|null $previous = null)
     {
-        return parent::__construct($message, $code, $previous);
+        parent::__construct($message, $code, $previous);
     }
 }
